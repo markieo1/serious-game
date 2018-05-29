f4894af7737731fdc008cb87d725f2be4a9a5626
Marco <marcohavermans+github@hotmail.nl>
2018-05-29 10:41:47 +0200

    Created FoodInteractable script.

---
+++ Assets/Scripts/Interaction/Food/FoodInteractable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodInteractable : InteractableBase
+{
+	public GameObject player;
+	public int carbohydrates;
+
+	public override void OnInteract()
+	{
+		// TODO: Implement the interaction possibilties
+		throw new System.NotImplementedException();
+	}
+}
