26fdaa4316918f61f7ee0adab54de528cd18e1a1
Unknown <kly1@avans.nl>
2018-05-29 11:43:46 +0200

    Added some logic to load/unload screens. Implementation missing

--- Assets/MainMenu/Scripts/StartGame.cs
+++ Assets/MainMenu/Scripts/StartGame.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
-public class StartGame : MonoBehaviour {
+public class StartGame : MonoBehaviour
+{
+	private Scene _gameScene;
+	private Scene _loadScene;
 
 	public void Start_Click()
 	{
-		Debug.Log("Started");
+		// TODO: Modify Screennames (loading and entry screen)
+		_gameScene = SceneManager.GetSceneByName("GameScene");
+		_loadScene = SceneManager.GetSceneByName("LoadScene");
+	}
+
+	private void Update()
+	{
+		if (_gameScene.isLoaded)
+		{
+			SceneManager.UnloadSceneAsync(_loadScene);
+		}
 	}
 }
