f84add8f4ebbe799baf5740b297647f9d5d81213
Richard Bos <richard.bos93@gmail.com>
2018-05-30 14:21:45 +0200

    Changed Loading Scene

---
+++ Assets/Rooms/Loading/Scripts/Loading.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+	private LoadingManager loadingManager;
+	private string sceneName;
+
+	// Use this for initialization
+	void Start()
+	{
+		loadingManager = LoadingManager.Instance;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (loadingManager != null)
+		{
+			try
+			{
+				sceneName = loadingManager.GetNextScene();
+
+				StartCoroutine(LoadSceneAsync(sceneName));
+			}
+			catch { }
+		}
+	}
+
+	private IEnumerator LoadSceneAsync(string sceneName)
+	{
+		yield return new WaitForSeconds(1);
+
+		AsyncOperation loadingScene = SceneManager.LoadSceneAsync(sceneName);
+
+		while (!loadingScene.isDone)
+		{
+			yield return null;
+		}
+	}
+}
