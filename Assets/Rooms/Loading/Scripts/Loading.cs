3155805410f8f3c97f613ee83b8780bcd330dd7c
Richard Bos <richard.bos93@gmail.com>
2018-05-31 09:46:54 +0200

    Changed Loading (added feedback #2)

--- Assets/Rooms/Loading/Scripts/Loading.cs
+++ Assets/Rooms/Loading/Scripts/Loading.cs
@@ -5,25 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Loading : MonoBehaviour
 {
-	private string sceneName;
-
-	// Use this for initialization
-	void Start()
-	{
-	}
 
 	// Update is called once per frame
 	void Update()
 	{
 		try
 		{
-			sceneName = LoadingManager.Instance.GetNextScene();
+			string sceneName = LoadingManager.Instance.GetNextScene();
 
 			StartCoroutine(LoadSceneAsync(sceneName));
 		}
 		catch
 		{
-			Debug.Log("Loading screen started without next scene.");
+			Debug.LogError("Loading screen started without next scene.");
 		}
 	}
 
