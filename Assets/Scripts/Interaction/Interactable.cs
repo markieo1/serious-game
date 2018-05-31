15f4a80d710828e055ce1a9590b5a3d2543256ae
Marco <marcohavermans+github@hotmail.nl>
2018-05-31 11:34:23 +0200

    Re-added trigger checks for interactable.

--- Assets/Scripts/Interaction/Interactable.cs
+++ Assets/Scripts/Interaction/Interactable.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
@@ -8,4 +10,36 @@ public class Interactable : MonoBehaviour
 	/// The interactions
 	/// </summary>
 	public Interaction[] Interactions = new Interaction[0];
+
+	/// <summary> 
+	/// Gets or sets the collider. 
+	/// </summary> 
+	private Collider Collider { get; set; }
+
+	private void Start()
+	{
+		// This way we ensure there is an collider 
+		Collider[] colliders = GetComponents<Collider>();
+
+		if (colliders.Length <= 0)
+		{
+			throw new NotSupportedException(string.Format("An collider is required to support interactable, object: {0}", name));
+		}
+
+		if (!colliders.Any(x => x.isTrigger))
+		{
+			throw new NotSupportedException(string.Format("An collider as trigger is required to support interactable, object: {0}", name));
+		}
+
+		Collider = colliders.FirstOrDefault(x => x.isTrigger);
+
+		if (!Collider)
+		{
+			throw new NotSupportedException(string.Format("Collider configured as trigger was not found, object: {0}", name));
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+	}
 }
