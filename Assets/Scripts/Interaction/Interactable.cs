2b31fd48a21c98db7d1661112d208b940bd8f6fa
Marco <marcohavermans+github@hotmail.nl>
2018-05-31 11:27:28 +0200

    Created interaction / reaction logic.

---
+++ Assets/Scripts/Interaction/Interactable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+	/// <summary>
+	/// The interactions
+	/// </summary>
+	public Interaction[] Interactions = new Interaction[0];
+}
---
+++ Assets/Scripts/Interaction/ReactionCollection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactionCollection : MonoBehaviour
+{
+	/// <summary>
+	/// The reactions
+	/// </summary>
+	public Reaction[] reactions = new Reaction[0];
+
+	private void Start()
+	{
+		for (int i = 0; i < reactions.Length; i++)
+		{
+			reactions[i].Init();
+		}
+	}
+
+	/// <summary>
+	/// Reacts.
+	/// </summary>
+	public void React()
+	{
+		for (int i = 0; i < reactions.Length; i++)
+		{
+			reactions[i].React(this);
+		}
+	}
+}
