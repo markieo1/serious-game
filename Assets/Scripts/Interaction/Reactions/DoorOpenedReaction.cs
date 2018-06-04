224c9c22ffb1c91eb39753780b15f688f6b7a6d5
Richard Bos <richard.bos93@gmail.com>
2018-06-04 14:09:10 +0200

    Added Spawn Feature (not finished)

---
+++ Assets/Scripts/Interaction/Reactions/DoorOpenedReaction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorOpenedReaction : Reaction
+{
+	[Scene]
+	private string loadingScene = "Loading";
+
+	[Scene]
+	private string newScene = "Hallway";
+
+	private string spawnPoint = "SpawnA";
+
+	public override void React(MonoBehaviour monoBehaviour)
+	{
+		SceneManager.LoadScene(loadingScene, LoadSceneMode.Single);
+		LoadingManager.Instance.SetSceneToLoad(newScene);
+		LoadingManager.Instance.SetSpawnPoint(spawnPoint);
+	}
+}
