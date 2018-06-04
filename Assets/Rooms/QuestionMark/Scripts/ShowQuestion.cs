e0c1a599a245e8987e6861bb35818c10741b2337
DanielvanBavel <daniel.ajax.1996@hotmail.com>
2018-06-04 15:30:44 +0200

    first setup question mark on button pressed

---
+++ Assets/Rooms/QuestionMark/Scripts/ShowQuestion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowQuestion : MonoBehaviour {
+
+    public Text question;
+    public string questionText = "";
+    public float time = 5;
+
+    void Start()
+    {
+        Text txt = question.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if(Input.GetButton("Help"))
+        {
+            
+            DisplayText();
+        }
+    }
+
+    void DisplayText()
+    {
+        question.text = questionText;
+        //yield return new WaitForSeconds(time);
+    }
+}
