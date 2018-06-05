aad99057d6e722a3999d42f89f5625509a50437a
Marco <marcohavermans+github@hotmail.nl>
2018-06-05 13:32:48 +0200

    Started working on displaying popups.

---
+++ Assets/Scripts/HUD/Popup/PopupController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupController : MonoBehaviour
+{
+	/// <summary>
+	/// The text
+	/// </summary>
+	public Text Text;
+
+	/// <summary>
+	/// The canvas group
+	/// </summary>
+	public CanvasGroup CanvasGroup;
+
+	private void OnEnable()
+	{
+		EventManager.StartListening<ShowPopupEvent>(OnShowPopup);
+		EventManager.StartListening<ClosePopupEvent>(OnClosePopup);
+	}
+
+	private void OnDisable()
+	{
+		EventManager.StopListening<ShowPopupEvent>(OnShowPopup);
+		EventManager.StopListening<ClosePopupEvent>(OnClosePopup);
+	}
+
+	/// <summary>
+	/// Called when [show popup].
+	/// </summary>
+	/// <param name="event">The event.</param>
+	private void OnShowPopup(ShowPopupEvent @event)
+	{
+		DisplayPopupItem(@event.Item);
+	}
+
+	/// <summary>
+	/// Called when [close popup].
+	/// </summary>
+	/// <param name="event">The event.</param>
+	private void OnClosePopup(ClosePopupEvent @event)
+	{
+		HidePopupItems();
+	}
+
+	/// <summary>
+	/// Displays the popup item.
+	/// </summary>
+	/// <param name="popupItem">The popup item.</param>
+	private void DisplayPopupItem(PopupItem popupItem)
+	{
+		StartCoroutine(WaitAndDisplay(popupItem));
+	}
+
+	/// <summary>
+	/// Hides the popup items.
+	/// </summary>
+	private void HidePopupItems()
+	{
+		StopAllCoroutines();
+		HidePopup();
+	}
+
+	/// <summary>
+	/// Waits and display the popupitem.
+	/// </summary>
+	/// <param name="popupItem">The popup item.</param>
+	/// <returns></returns>
+	private IEnumerator WaitAndDisplay(PopupItem popupItem)
+	{
+		SetPopupText(popupItem.Text);
+
+		if (popupItem.DelayInSeconds > 0)
+		{
+			yield return new WaitForSeconds(popupItem.DelayInSeconds);
+		}
+
+		ShowPopup();
+
+		// Larger than 0 means we have a limit, else we display indefenitly.
+		if (popupItem.DisplayTimeInSeconds > 0)
+		{
+			yield return new WaitForSeconds(popupItem.DisplayTimeInSeconds);
+
+			HidePopup();
+		}
+	}
+
+	/// <summary>
+	/// Sets the popup text.
+	/// </summary>
+	/// <param name="text">The text.</param>
+	private void SetPopupText(string text)
+	{
+		Text.text = text;
+	}
+
+	/// <summary>
+	/// Shows the popup.
+	/// </summary>
+	private void ShowPopup()
+	{
+		CanvasGroup.alpha = 1;
+	}
+
+	/// <summary>
+	/// Hides the popup.
+	/// </summary>
+	private void HidePopup()
+	{
+		CanvasGroup.alpha = 0;
+	}
+}
---
+++ Assets/Scripts/HUD/Popup/PopupItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupItem
+{
+	/// <summary>
+	/// The delay in seconds
+	/// </summary>
+	public float DelayInSeconds = 0;
+
+	/// <summary>
+	/// The display time in seconds
+	/// </summary>
+	public float DisplayTimeInSeconds = 0;
+
+	/// <summary>
+	/// The text
+	/// </summary>
+	public string Text;
+}
