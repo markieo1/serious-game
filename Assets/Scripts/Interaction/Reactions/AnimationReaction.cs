0ce8704e26f2bc54dfa4a21762a91c1b88b4426d
Unknown <kenwai2010@gmail.com>
2018-06-06 15:19:38 +0200

    fix open fence

---
+++ Assets/Scripts/Interaction/Reactions/AnimationReaction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Reactions/Animation")]
+public class AnimationReaction : Reaction
+{
+	private Animator anim;
+
+	public string trigger;
+
+	protected override void SpecificInit(MonoBehaviour monoBehaviour)
+	{
+		base.SpecificInit(monoBehaviour);
+
+		ReactionCollection collection = monoBehaviour.GetComponent<ReactionCollection>();
+
+		anim = collection.AnimationSource;
+
+		if (!anim)
+		{
+			throw new NotSupportedException("An animator on : \"" + monoBehaviour.name + "\" is required for the animation to work.");
+		}
+
+	}
+
+	public override void React()
+	{
+		if (string.IsNullOrEmpty(trigger))
+		{
+			throw new ArgumentNullException("Please specify a trigger");
+		}
+
+		anim.SetTrigger(trigger);
+	}
+}
