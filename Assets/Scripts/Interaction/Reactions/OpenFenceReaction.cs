54767b4961ab442ff3bfde09a3d58a149bba281d
Unknown <kenwai2010@gmail.com>
2018-06-06 09:03:34 +0200

    /open-fence:
    open fence wip

---
+++ Assets/Scripts/Interaction/Reactions/OpenFenceReaction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Reactions/OpenFence")]
+public class OpenFenceReaction : Reaction
+{
+
+	public override void React(MonoBehaviour monoBehaviour)
+	{
+		Debug.Log("OpenFence reaction has been called");
+	}
+}
