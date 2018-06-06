12db80a09dec60b70cc7cdd3f8ec4e165d76facd
DanielvanBavel <daniel.ajax.1996@hotmail.com>
2018-06-06 16:34:24 +0200

    updates

---
+++ Assets/Scripts/QuestionMark/ShowQuestion.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowQuestion : MonoBehaviour
+{
+    public Text question;
+    public string questionText = "";
+    public Transform target;
+    public LayerMask layermask;
+
+    void Start()
+    {
+        question = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        //Debug.Log(Physics.Linecast(transform.position, target.position, layermask.value));
+
+        var MirrorObj = Physics.Linecast(transform.position, target.position, layermask.value);
+        if(MirrorObj)
+        {
+            question.text = questionText;
+        }
+    }
+}
