8bf6c2591750c57c3582ea1cb4b5eeed41702d83
Marco <marcohavermans+github@hotmail.nl>
2018-06-07 11:52:05 +0200

    Used extensions methods for hiding / showing canvas group.

--- Assets/Scripts/Utilities/CanvasGroupExtensions.cs
+++ Assets/Scripts/Utilities/CanvasGroupExtensions.cs
@@ -25,4 +25,18 @@ public static class CanvasGroupExtensions
 		canvasGroup.interactable = true;
 		canvasGroup.blocksRaycasts = true;
 	}
+
+	/// <summary>
+	/// Determines whether this instance is visible.
+	/// </summary>
+	/// <param name="canvasGroup">The canvas group.</param>
+	/// <returns>
+	///   <c>true</c> if the specified canvas group is visible; otherwise, <c>false</c>.
+	/// </returns>
+	public static bool IsVisible(this CanvasGroup canvasGroup)
+	{
+		return canvasGroup.alpha == 1
+			&& canvasGroup.interactable == true
+			&& canvasGroup.blocksRaycasts == true;
+	}
 }
