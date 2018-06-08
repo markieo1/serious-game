fbddd55ae060fbaa8234ceeeaf5bee24d0c07d85
DanielvanBavel <daniel.ajax.1996@hotmail.com>
2018-06-08 11:46:48 +0200

    setup mirror collider in new branch

---
+++ Assets/Scripts/Hints/ShowHint.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHint : MonoBehaviour {
+
+    public Text question;
+    public string questionText = "";
+    private bool showtext;
+
+	void Start () {
+        question.GetComponent<Text>();
+	}
+	
+	void Update () {
+        if (showtext)
+        {
+            question.text = questionText;
+        }
+        else
+        {
+            question.text = "";
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            showtext = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            showtext = false;
+        }
+    }
+}
