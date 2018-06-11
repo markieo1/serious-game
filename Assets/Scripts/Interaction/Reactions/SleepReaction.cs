51c119b53a1d2d24e272fe0092a80f4dacb8393c
Marco <marcohavermans+github@hotmail.nl>
2018-06-11 14:40:37 +0200

    Made fade and sleep reactions work in bedroom.

--- Assets/Scripts/Interaction/Reactions/SleepReaction.cs
+++ Assets/Scripts/Interaction/Reactions/SleepReaction.cs
@@ -6,6 +6,21 @@ using UnityEngine;
 public class SleepReaction : Reaction
 {
 	/// <summary>
+	/// The start reaction
+	/// </summary>
+	public FadeReaction StartReaction;
+
+	/// <summary>
+	/// The end reaction
+	/// </summary>
+	public FadeReaction EndReaction;
+
+	/// <summary>
+	/// The sleep duration
+	/// </summary>
+	public float SleepDuration;
+
+	/// <summary>
 	/// The player
 	/// </summary>
 	private PlayerController Player;
@@ -16,10 +31,26 @@ public class SleepReaction : Reaction
 
 		// Find the player in the scene
 		Player = PlayerController.GetPlayer();
+
+		// Init the other reactions
+		StartReaction.Init(monoBehaviour);
+		EndReaction.Init(monoBehaviour);
 	}
 
 	public override void React()
 	{
+		// Since we now know how long the start reaction is we can correctly end it
+		StartReaction.React();
+
+		MonoBehaviour.StartCoroutine(EndSleepCoroutine(SleepDuration));
+
 		Player.Sleep();
+	}
+
+	private IEnumerator EndSleepCoroutine(float sleepDuration)
+	{
+		yield return new WaitForSecondsRealtime(sleepDuration);
+
+		EndReaction.React();
 	}
 }
