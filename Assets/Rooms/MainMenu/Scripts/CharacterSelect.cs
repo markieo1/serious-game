29e84b39f2d0f1564867418307676dd428655abc
DanielvanBavel <daniel.ajax.1996@hotmail.com>
2018-06-13 13:36:22 +0200

    Updated women character with correct spawn

--- Assets/Rooms/MainMenu/Scripts/CharacterSelect.cs
+++ Assets/Rooms/MainMenu/Scripts/CharacterSelect.cs
@@ -5,25 +5,14 @@ using UnityEngine;
 public class CharacterSelect : MonoBehaviour
 {
     public GameObject PlayerToPlay;
-    public GameObject PlayerNoPlay;
 
 	public void SelectBoy()
 	{
-		CharacterData.CharacterSelection = CharacterSelection.MALE;
-        if (PlayerToPlay.gameObject.name == "BoyPlayer")
-        {
-            PlayerToPlay.gameObject.SetActive(true);
-        }
-        PlayerNoPlay.gameObject.SetActive(false);
+		CharacterData.CharacterSelection = CharacterSelection.MALE;     
     }
 
 	public void SelectGirl()
 	{
 		CharacterData.CharacterSelection = CharacterSelection.FEMALE;
-        if (PlayerToPlay.gameObject.name == "GirlPlayer")
-        {
-            PlayerToPlay.gameObject.SetActive(true);
-        }
-        PlayerNoPlay.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Scene/SpawnManager.cs
+++ Assets/Scripts/Scene/SpawnManager.cs
@@ -6,74 +6,85 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
 	public float TimeToSpawnInSeconds = 10;
+    public GameObject BoyPlayer;
+    public GameObject GirlPlayer;
 
-	public GameObject Player;
-	public List<GameObject> SpawnPoints;
-
+    public List<GameObject> SpawnPoints;
 	public List<Transform> TransformPoints;
 	public List<GameObject> FoodObjects;
 
-    private CharacterSelect PlayerToLoad = new CharacterSelect();
+    private GameObject Player;
 
     Dictionary<Transform, Interactable> TransformPointsAndFoodObjects;
 
 	// Use this for initialization
 	void Start()
 	{
-		TransformPointsAndFoodObjects = new Dictionary<Transform, Interactable>();
+        var PlayerCharacter = (int)CharacterData.CharacterSelection;
 
-		// loop through points insert in dictionary
-		TransformPoints.ForEach(x =>
-		{
-			TransformPointsAndFoodObjects.Add(x, null);
-		});
+        if(PlayerCharacter == 0)
+        {
+            Player = BoyPlayer;
+            Instantiate(Player);
+        }
+        if(PlayerCharacter == 1)
+        {
+            Player = GirlPlayer;
+            Instantiate(Player);
+        }
+
+        TransformPointsAndFoodObjects = new Dictionary<Transform, Interactable>();
+
+        // loop through points insert in dictionary
+        TransformPoints.ForEach(x =>
+        {
+            TransformPointsAndFoodObjects.Add(x, null);
+        });
 
 
-		if (LoadingManager.Instance.GetSpawnCheck())
-		{
-			string spawnTag = LoadingManager.Instance.GetSpawnPoint();
+        if (LoadingManager.Instance.GetSpawnCheck())
+        {
+            string spawnTag = LoadingManager.Instance.GetSpawnPoint();
 
-			GameObject spawnObject = SpawnPoints.FirstOrDefault(s => s.tag == spawnTag);
+            GameObject spawnObject = SpawnPoints.FirstOrDefault(s => s.tag == spawnTag);
 
             if (spawnObject != null)
-			{
-				SetPlayerToSpawn(PlayerToLoad.PlayerToPlay, spawnObject);
-			}
-		}
+            {
+                SetPlayerToSpawn(Player, spawnObject);
+            }
+        }
 
-		if (TransformPoints != null && TransformPoints.Count > 0 && FoodObjects != null && FoodObjects.Count > 0)
-		{
-			InvokeRepeating("CheckAndSpawnFood", 0, TimeToSpawnInSeconds);
-		}
-	}
+        if (TransformPoints != null && TransformPoints.Count > 0 && FoodObjects != null && FoodObjects.Count > 0)
+        {
+            InvokeRepeating("CheckAndSpawnFood", 0, TimeToSpawnInSeconds);
+        }
+    }
 
-	public void CheckAndSpawnFood()
-	{
-		// Check in dictionary if value is null, if so spawn random
+    public void CheckAndSpawnFood()
+    {
+        // Check in dictionary if value is null, if so spawn random
 
-		for (int i = 0; i < TransformPointsAndFoodObjects.Count; i++)
-		{
-			var x = TransformPointsAndFoodObjects.ElementAtOrDefault(i);
-			if (x.Value)
-			{
-				continue;
-			}
-			else
-			{
-				int randomNumber = Random.Range(0, FoodObjects.Count);
-				var t = Instantiate(FoodObjects[randomNumber], x.Key.position, Quaternion.identity);
-				Interactable comp = t.GetComponent<Interactable>();
-				TransformPointsAndFoodObjects[x.Key] = comp;
-			}
-		}
-	}
+        for (int i = 0; i < TransformPointsAndFoodObjects.Count; i++)
+        {
+            var x = TransformPointsAndFoodObjects.ElementAtOrDefault(i);
+            if (x.Value)
+            {
+                continue;
+            }
+            else
+            {
+                int randomNumber = Random.Range(0, FoodObjects.Count);
+                var t = Instantiate(FoodObjects[randomNumber], x.Key.position, Quaternion.identity);
+                Interactable comp = t.GetComponent<Interactable>();
+                TransformPointsAndFoodObjects[x.Key] = comp;
+            }
+        }
+    }
 
-	public void SetPlayerToSpawn(GameObject player, GameObject spawn)
-	{
-        PlayerToLoad.PlayerToPlay = player;
-
+    public void SetPlayerToSpawn(GameObject player, GameObject spawn)
+    {
         // Set the current player position and rotation
-        PlayerToLoad.PlayerToPlay.transform.position = spawn.transform.position;
-        PlayerToLoad.PlayerToPlay.transform.rotation = spawn.transform.rotation;
-	}
+        player.transform.position = spawn.transform.position;
+        player.transform.rotation = spawn.transform.rotation;
+    }
 }
